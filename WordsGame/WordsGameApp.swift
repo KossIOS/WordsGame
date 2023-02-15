bda4a871e78f7e3b583edf3180a8780cef2eb100
KossIOS <kossioskoroban@gmil.com>
2023-02-15 18:47:31 +0100

    Initial Commit

---
+++ WordsGame/WordsGameApp.swift
@@ -0,0 +1,18 @@
+//
+//  WordsGameApp.swift
+//  WordsGame
+//
+//  Created by Konstantyn Koroban on 15/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordsGameApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            StartView()
+        }
+    }
+}
